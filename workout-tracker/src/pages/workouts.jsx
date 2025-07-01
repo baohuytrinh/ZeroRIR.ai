64d9edf0cd0e44b8983c8501fdb587e34b0441cc
Bao-Huy Trinh <anthonytrinh0508@gmail.com>
2025-07-01 00:05:55 -0400

    if else

--- workout-tracker/src/pages/workouts.jsx
+++ workout-tracker/src/pages/workouts.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
+
 function Workouts(){
-    return <div className="workouts-empty">
-        <h2>No workouts yet</h2>
-        <p>start adding workouts, and they'll appear here</p>
 
-    </div>
+    const [workouts, setWorkouts] = useState("");
 
+
+
+    return (
+        
+        <div>
+            {workouts.length === 0 ?  (
+                <div className="workouts-empty">
+                    <h2>No workouts yet</h2>
+                    <p>start adding workouts, and they'll appear here</p>
+                </div>
+            ) : (
+                <div className="workouts-chart">
+                    <h2>Your Workout(s)</h2>
+                    <p>start adding workouts, and they'll appear here</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 
